1205c97bf7a677fa919d642a686f2b4468afb84f
Lam <lam@foresightmobile.com>
2023-03-16 13:21:08 +0100

    Added API key.

--- lib/utils/constants/strings.dart
+++ lib/utils/constants/strings.dart
@@ -3,7 +3,7 @@ const String appTitle = 'My Flutter Application';
 
 // Networking and APIs
 const String baseUrl = 'https://newsapi.org/v2';
-const String defaultApiKey = 'API_KEY';
+const String defaultApiKey = 'a03191ee70f94db5a26adbad2f27f223';
 
 // Storage and Databases
 const String articlesTableName = 'article_table';
